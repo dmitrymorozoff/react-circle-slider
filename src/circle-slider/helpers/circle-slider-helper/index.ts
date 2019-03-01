da89e64423818b7c6bc1da61e7a574bd21aa2350
kesav <14555834+kesavamoorthi@users.noreply.github.com>
2019-03-01 18:27:13 +0530

    fixed  a minor issue to add IE11 compatibility

--- src/circle-slider/helpers/circle-slider-helper/index.ts
+++ src/circle-slider/helpers/circle-slider-helper/index.ts
@@ -16,9 +16,11 @@ export class CircleSliderHelper {
     }
 
     public getEpsilon() {
-        var e = 1.0;
+        let e = 1.0;
         while ( ( 1.0 + 0.5 * e ) !== 1.0 )
+        {
           e *= 0.5;
+        }
         return e;
     }
 
